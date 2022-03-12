c0f645cded3ca84d363e2a7800f1ec789dd05f47
hyvip-ai <rm2932002@gmail.com>
2022-03-12 19:44:25 +0530

    Dummy Data Changes for testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        color="#e15f41"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <Link href="/contributors">Contributors</Link>;
 }
